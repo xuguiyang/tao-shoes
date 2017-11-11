17caa13f576e080ad52ea8c205957bae348fc322
yangyang <xuguiyang0211@foxmail.com>
2017-11-11 19:21:58 +0800

    乐淘4

---
+++ letao4/js/productManage.js
@@ -0,0 +1,20 @@
+$(function(){
+  var getProductData = function(pageNum){
+    $.ajax({
+      type:'get',
+      url:' /product/queryProductDetailList',
+      data:{
+        page:pageNum||1,
+        pageSize: 5
+      },
+      success:function(data){
+        // console.log(data);
+        var productResult = template('product-template',data);
+        $('tbody').html(productResult);
+      }
+    })
+  }
+
+
+  getProductData();
+})
